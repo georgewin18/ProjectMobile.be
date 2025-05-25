dd5326d1049c7705066e3101168fbc6f0e517905
georgewin18 <georgewk1809@gmail.com>
2025-05-25 20:50:02 +0700

    fix transaction route

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { getTransactions, createTransaction } from '../controllers/transactionController'
+import { requireAuth } from '../middleware/requireAuth'
 
 const router = Router()
+
+router.use((req, res, next) => requireAuth(req, res, next))
 
 router.get('/', getTransactions)
 router.post('/', createTransaction)
