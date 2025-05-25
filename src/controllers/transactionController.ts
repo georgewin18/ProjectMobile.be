bfce25d7d85e5eca157691635a640f7d1ca37ec9
georgewin18 <georgewk1809@gmail.com>
2025-05-25 20:51:03 +0700

    change transaction controller: user middleware

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -2,30 +2,6 @@ import { Request, Response } from 'express'
 import { AuthenticatedRequest } from '../middleware/requireAuth'
 import db from '../db'
 
-// export const getTransactions = async (_req: Request, res: Response) => {
-//   const transactions = await db.selectFrom('transactions').selectAll().execute()
-//   res.json(transactions)
-// }
-
-// export const createTransaction = async (req: Request, res: Response) => {
-//   const { user_id, category_id, amount, description, transaction_date } = req.body
-
-//   const inserted = await db
-//     .insertInto('transactions')
-//     .values({
-//       user_id,
-//       category_id,
-//       amount,
-//       description,
-//       transaction_date
-//     })
-//     .returningAll()
-//     .executeTakeFirst()
-
-//   res.status(201).json(inserted)
-// }
-
-
 export const createTransaction = async (req: AuthenticatedRequest, res: Response) => {
   const { category_id, amount, description, transaction_date } = req.body
   const user_id = req.user!.id
