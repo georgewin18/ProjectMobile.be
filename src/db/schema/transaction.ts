938cd5ff36e1a29c9c97007106699a72b3b9f721
georgewin18 <georgewk1809@gmail.com>
2025-05-25 20:48:42 +0700

    minor transaction schema change

--- src/db/schema/transaction.ts
+++ src/db/schema/transaction.ts
@@ -1,8 +1,10 @@
 export interface Transaction {
   id?: string
   user_id: string
+  category_id: number
   amount: number
-  category: string
+  description: string
+  transaction_date: string
   created_at?: string
 }
   
