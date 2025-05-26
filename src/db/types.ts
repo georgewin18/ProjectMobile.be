82bcbc7919cf16a04e1ff6dee5ded52ce5791650
georgewin18 <georgewk1809@gmail.com>
2025-05-26 21:38:04 +0700

    remove unused schema

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,9 +1,7 @@
 import { Budget } from './schema/budget'
 import { Transaction } from './schema/transaction'
-import { User } from './schema/user'
 
 export interface Database {
-  users: User
   transactions: Transaction
   budgets: Budget
 }
