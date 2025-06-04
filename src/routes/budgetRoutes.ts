474dc943756ab64c9120c60c562828ab855e2d05
georgewin18 <georgewk1809@gmail.com>
2025-06-04 10:16:03 +0700

    use middleware in budgets route

--- src/routes/budgetRoutes.ts
+++ src/routes/budgetRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { getBudgets, createBudget } from '../controllers/budgetController'
+import { requireAuth } from '../middleware/requireAuth'
 
 const router = Router()
+
+router.use((req, res, next) => requireAuth(req, res, next))
 
 router.get('/', getBudgets)
 router.post('/', createBudget)
