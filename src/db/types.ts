ea4c753a898934dd5fce0b4f85aa1655180efa9f
georgewin18 <georgewk1809@gmail.com>
2025-06-05 17:32:49 +0700

    rollback

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,35 +1,7 @@
-import { ColumnType, Generated } from "kysely";
-
-export interface TransactionTable {
-  id: Generated<number>;
-  user_id: string;
-  category_id: number;
-  amount: string;
-  description: string;
-  transaction_date: Date;
-  created_at: ColumnType<Date, undefined, undefined>;
-}
-
-export interface BudgetsTable {
-  id: Generated<number>;
-  user_id: string;
-  category_id: number;
-  amount: string;
-  start_date: Date;
-  end_date: Date;
-  created_at: ColumnType<Date, undefined, undefined>;
-}
-
-export interface BudgetUsagesTable {
-  id: Generated<number>;
-  budget_id: number;
-  transaction_id: number;
-  used_amount: string;
-  created_at: ColumnType<Date, undefined, undefined>;
-}
+import { Budget } from './schema/budget'
+import { Transaction } from './schema/transaction'
 
 export interface Database {
-  transactions: TransactionTable;
-  budgets: BudgetsTable;
-  budget_usages: BudgetUsagesTable;
+  transactions: Transaction
+  budgets: Budget
 }
